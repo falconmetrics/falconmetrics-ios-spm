b67d327b7488d9a3f5dc7dc5b653bf4381e04a08
Remon <remonhelmond+dev@gmail.com>
2025-06-08 09:45:46 +0100

    chore: releae 0.1.4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FalconMetrics",
-            url: "https://falconmetrics-sdk.s3.eu-west-1.amazonaws.com/ios/0.1.3/FalconMetricsSDK.xcframework.zip",
-            checksum: "9eaf24ff88944bdcfa51ded7c0fc04e1dea3610e0b1a22d65e871217dcb3089c"
+            url: "https://falconmetrics-sdk.s3.eu-west-1.amazonaws.com/ios/0.1.4/FalconMetricsSDK.xcframework.zip",
+            checksum: "9a045564ec9f50259db77ff28f33d3edb5ba796f42312817dec2803c84537eb6"
         )
     ]
 )
